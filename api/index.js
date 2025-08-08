d95fcbaa0a90ef3c6f263928a4a9a75d40f047be
riesdepies <riesdepies@gmail.com>
2025-08-09 00:18:00 +0200

    Fix: gebruik addonInterface.router ipv serveHTTP

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
-const { serveHTTP } = require('stremio-addon-sdk');
-const addonInterface = require('../addon.js'); // Gaat één map omhoog om addon.js te vinden
+const addonInterface = require('../addon.js');
 
-// Deze handler wordt door Vercel aangeroepen voor elk request
+// De handler die Vercel aanroept
 const handler = (req, res) => {
-    serveHTTP(addonInterface, { req, res });
+  // De addonInterface heeft een ingebouwde router die zich gedraagt als Express-middleware.
+  // We roepen deze direct aan en geven de request en response objecten door.
+  addonInterface.router(req, res, () => {
+    // Dit is de 'next' functie, die wordt aangeroepen als de router de request niet herkent.
+    // Dit zou niet moeten gebeuren, maar we eindigen de request netjes.
+    res.statusCode = 404;
+    res.end('Not Found');
+  });
 };
 
 module.exports = handler;
