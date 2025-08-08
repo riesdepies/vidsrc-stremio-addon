b72f32ca780a53746c1984010f2430e1bdc78d55
riesdepies <riesdepies@gmail.com>
2025-08-09 01:40:28 +0200

    Definitieve Fix: implementeer officiële serverless methode met getRouter()

--- api/index.js
+++ api/index.js
@@ -1,15 +1,14 @@
+const { getRouter } = require('stremio-addon-sdk');
 const addonInterface = require('../addon.js');
 
-// De handler die Vercel aanroept
-const handler = (req, res) => {
-  // De addonInterface heeft een ingebouwde router die zich gedraagt als Express-middleware.
-  // We roepen deze direct aan en geven de request en response objecten door.
-  addonInterface.router(req, res, () => {
-    // Dit is de 'next' functie, die wordt aangeroepen als de router de request niet herkent.
-    // Dit zou niet moeten gebeuren, maar we eindigen de request netjes.
+// Maak de router aan door de interface aan de getRouter functie te geven
+const router = getRouter(addonInterface);
+
+// Exporteer de handler die Vercel aanroept
+module.exports = (req, res) => {
+  router(req, res, () => {
+    // Fallback voor als de router de request niet herkent
     res.statusCode = 404;
     res.end('Not Found');
   });
 };
-
-module.exports = handler;
