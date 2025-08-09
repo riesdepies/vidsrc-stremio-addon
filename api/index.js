1283e71b458698cf88aa2fbbc6f2aa1c7a027f9c
riesdepies <riesdepies@gmail.com>
2025-08-09 04:17:47 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const router = getRouter(addonInterface);
-const logoPath = path.join(__dirname, '..', 'logo.svg');
+const logoPath = path.join(__dirname, '..', 'logo.png');
 
 module.exports = (req, res) => {
   // Voeg CORS headers toe
@@ -13,8 +13,8 @@ module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
   
   // Serveer het logo als daarom wordt gevraagd
-  if (req.url === '/logo.svg') {
-    res.setHeader('Content-Type', 'image/svg+xml');
+  if (req.url === '/logo.png') {
+    res.setHeader('Content-Type', 'image/png');
     fs.createReadStream(logoPath).pipe(res);
     return;
   }
