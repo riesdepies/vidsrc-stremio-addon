3712eec2b635f40f2337a3b6ad2ab67199295c9c
riesdepies <riesdepies@gmail.com>
2025-08-09 02:02:12 +0200

    Final final fix: expliciet Content-Type header toevoegen

--- api/index.js
+++ api/index.js
@@ -4,12 +4,13 @@ const addonInterface = require('../addon.js');
 const router = getRouter(addonInterface);
 
 module.exports = (req, res) => {
-  // Voeg hier de CORS headers toe
+  // Voeg ALLE vereiste headers toe
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
+  res.setHeader('Content-Type', 'application/json'); // <-- DE NIEUWE, CRUCIALE HEADER
 
-  // Behandel 'preflight' OPTIONS requests die browsers sturen voor CORS checks
+  // Behandel 'preflight' OPTIONS requests
   if (req.method === 'OPTIONS') {
     res.statusCode = 200;
     res.end();
