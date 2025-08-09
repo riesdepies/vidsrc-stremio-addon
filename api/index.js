b2242b1a4548ba85543f2adb093118fe31a9e2d4
riesdepies <riesdepies@gmail.com>
2025-08-09 05:23:49 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,28 +1,18 @@
 const { getRouter } = require('stremio-addon-sdk');
 const addonInterface = require('../addon.js');
-const fs = require('fs');
-const path = require('path');
 
 const router = getRouter(addonInterface);
-const logoPath = path.join(__dirname, '..', 'logo.png');
 
 module.exports = (req, res) => {
-  // Voeg CORS headers toe
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
-  
-  // Serveer het logo als daarom wordt gevraagd
-  if (req.url === '/logo.png') {
-    res.setHeader('Content-Type', 'image/png');
-    fs.createReadStream(logoPath).pipe(res);
-    return;
-  }
-  
-  // Stuur andere requests door naar de addon router
-  res.setHeader('Content-Type', 'application/json');
-  router(req, res, () => {
-    res.statusCode = 404;
-    res.end('Not Found');
-  });
+    // Voeg CORS headers toe
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
+
+    // Stuur alle requests door naar de addon router
+    router(req, res, () => {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ err: 'Not Found' }));
+    });
 };
