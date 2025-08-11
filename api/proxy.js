af9dea882d6be7c79f8b64242a828f175b69fb75
riesdepies <riesdepies@gmail.com>
2025-08-11 04:56:48 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,20 +5,17 @@ module.exports = async (req, res) => {
     const targetUrl = req.query.url;
 
     if (!targetUrl) {
-        res.status(400).json({ error: 'URL parameter is missing' });
-        return;
+        return res.status(400).json({ error: 'URL parameter is missing' });
     }
 
     try {
-        // Gebruik de headers van de oorspronkelijke aanvraag,
-        // met name 'User-Agent' en 'Referer' zijn belangrijk.
+        // Stuur de headers van de oorspronkelijke aanvraag door.
         const response = await fetch(targetUrl, {
             headers: req.headers,
         });
 
         // Stuur de headers van het doel door naar de client.
         response.headers.forEach((value, name) => {
-            // Voorkom 'hop-by-hop' header fouten.
             if (!['content-encoding', 'transfer-encoding', 'connection'].includes(name.toLowerCase())) {
                 res.setHeader(name, value);
             }
