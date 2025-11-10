1a6728829077ff8cd73b3164545a5c1537d7e0db
riesdepies <riesdepies@gmail.com>
2025-11-10 21:01:48 +0100

    Update addon.js

--- addon.js
+++ addon.js
@@ -17,7 +17,8 @@ const manifest = {
     "idPrefixes": ["tt"]
 };
 
-const VIDSRC_DOMAINS = ["vidsrc.xyz", "vidsrc.in", "vidsrc.io", "vidsrc.me", "vidsrc.net", "vidsrc.pm", "vidsrc.vc", "vidsrc.to", "vidsrc.icu"];
+// --- AANGEPAST: Domeinen bijgewerkt naar de nieuwe VidSrc URLs ---
+const VIDSRC_DOMAINS = ["vidsrc-embed.ru", "vidsrc-embed.su", "vidsrcme.su", "vsrc.su"];
 
 // --- BROWSERPROFIELEN VOOR REALISTISCHE HEADERS ---
 const BROWSER_PROFILES = [
@@ -43,7 +44,7 @@ function getRandomBrowserProfile() {
 }
 
 const COMMON_HEADERS = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q-0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Language': 'en-US,en;q=0.9',
     'Accept-Encoding': 'gzip, deflate, br',
     'Upgrade-Insecure-Requests': '1',
@@ -161,4 +162,4 @@ builder.defineStreamHandler(async ({ type, id }) => {
     return Promise.resolve({ streams: [] });
 });
 
-module.exports = builder.getInterface();
+module.exports = builder.getInterface();```
